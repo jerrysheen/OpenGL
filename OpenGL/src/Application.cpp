717f8e5b5a95af6c11699651d3d6dfb9ae7177d2
jerrysheen <jerrysheen@gmail.com>
2021-07-13 15:06:45 +0800

    feat: rendering multiple object

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -20,6 +20,9 @@
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw_gl3.h"
+
 int main(void)
 {
     GLFWwindow* window;
@@ -48,10 +51,10 @@ int main(void)
 
 	{
 		float positions[] = {
-			100.0f, 100.0f, 0.0f, 0.0f,
-			200.0f, 100.0f, 1.0f, 0.0f,
-			200.0f, 200.0f, 1.0f, 1.0f,
-			 100.0f, 200.0f, 0.0f, 1.0f
+			-50.0f, -50.0f, 0.0f, 0.0f,
+			50.0f, -50.0f, 1.0f, 0.0f,
+			50.0f, 50.0f, 1.0f, 1.0f,
+			 -50.0f, 50.0f, 0.0f, 1.0f
 		};
 
 		unsigned int indices[] = {
@@ -72,16 +75,14 @@ int main(void)
 		IndexBuffer ib(indices, 6);
 
 		glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
-		glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 0, 0));
+		// camera projection
+		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
 
-		glm::mat4 mvp = proj * view * model;
 
 		Shader shader("res/shaders/Basic.shader");
 		shader.Bind();
 
 		shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
-		shader.SetUniformMat4f("u_MVP", mvp);
 		Texture texture("res/texture/Test.png");
 		texture.Bind();
 		shader.SetUniform1i("u_Texture", 0);
@@ -95,15 +96,40 @@ int main(void)
 		ib.Unbind();
 
 		Renderer render;
+
+		ImGui::CreateContext();
+		ImGui_ImplGlfwGL3_Init(window, true);
+		ImGui::StyleColorsDark();
+
+		glm::vec3 translationA = glm::vec3(200, 0, 0);
+		glm::vec3 translationB = glm::vec3(400, 200, 0);
+
 		while (!glfwWindowShouldClose(window))
 		{
 			/* Render here */
 			render.Clear();
 
+
+			ImGui_ImplGlfwGL3_NewFrame();
+
 			shader.Bind();
-			shader.SetUniform4f("u_Color", r, 0.5f, 0.8f, 1.0f);
+			//shader.SetUniform4f("u_Color", r, 0.5f, 0.8f, 1.0f);
 			texture.Bind();
-			render.Draw(va, ib, shader);
+
+			{
+				glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
+				glm::mat4 mvp = proj * view * model;
+				shader.SetUniformMat4f("u_MVP", mvp);
+				render.Draw(va, ib, shader);
+			}
+
+			{
+				glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
+				glm::mat4 mvp = proj * view * model;
+				shader.SetUniformMat4f("u_MVP", mvp);
+				render.Draw(va, ib, shader);
+			}
+
 
 			//GLClearError();
 			//glDrawArrays(GL_TRIANGLES, 0, 3);
@@ -111,7 +137,16 @@ int main(void)
 
 			//ASSERT(GLCheckError());
 			
+			{
 
+				ImGui::Text("Hello, world!");                           // Display some text (you can use a format string too)
+				ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
+				ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
+				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+			}
+
+			ImGui::Render();
+			ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
 
 			r = r > 1.0f ? 0.0f : r + 0.03f;
 			/* Swap front and back buffers */
@@ -121,6 +156,10 @@ int main(void)
 			glfwPollEvents();
 		}
 	}
+
+	ImGui_ImplGlfwGL3_Shutdown();
+	ImGui::DestroyContext();
+
     glfwTerminate();
     return 0;
 }
